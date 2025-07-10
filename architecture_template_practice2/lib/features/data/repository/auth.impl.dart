bdf5af7f9d858431369aed8aa08f98549108f813
EddyHezarian <mr.eddyhzn@gmail.com>
2025-07-10 14:14:25 +0330

    create data layer

---
+++ architecture_template_practice2/lib/features/data/repository/auth.impl.dart
@@ -0,0 +1,13 @@
+class AuthIMPL {
+
+  getUserInfo(int id){
+    // send Api Request
+    //! pass to Api Provider
+
+    // check if Data is Success
+
+    // convert Response to AuthEntity
+
+
+  }
+}
---
+++ architecture_template_practice2/lib/features/data/src/auth.api.dart
@@ -0,0 +1,7 @@
+class AuthApiProvider {
+  // get user data {
+  // url  = 'www.example.com';
+  //! response = (send Request)
+  // return response
+  //}
+}
